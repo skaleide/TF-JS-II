a7ecc276d1585fe4c7590d5517b92dcd4563f262
Lisandra Cruz <lisansouza@gmail.com>
2024-04-08 21:05:39 -0300

    codigo da aula 02

---
+++ aula02/index.js
@@ -0,0 +1,31 @@
+// instalar: npm install prompt-sync
+const prompt = require("prompt-sync")();
+
+// const nomeUsuario = prompt("Qual o seu nome? ");
+
+// for (let contador = 0; contador < 10; contador++) {
+//   console.log(nomeUsuario);
+// }
+
+/*
+const contador = 0; // contador
+contador < 10; // enquanto o contador for menor que 10
+contador++; // some mais um ao contador
+*/
+
+// let numero1 = parseInt(prompt("Digite um número"));
+// while (numero1 !== 10) {
+//   console.log("ops");
+//   numero1 = parseInt(prompt("Digite um número"));
+// }
+
+// let numero = prompt("Digite um número");
+
+// while (numero !== "10") {
+//   console.log("ops");
+//   numero = prompt("Digite um número");
+// }
+
+// do {
+//   numero = prompt("Digite um número");
+// } while (numero !== "10");
---
+++ aula02/repeticao.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")();
+
+/* const palavra = prompt("Digite uma palavra: ");
+//                      0123456789         19
+
+let palavraInvertida = "";
+for (let i = palavra.length - 1; i >= 0; i--) {
+  console.log(i + ": " + palavra[i]);
+  palavraInvertida += palavra[i];
+}
+console.log(palavraInvertida);
+*/
+
+// tamanho de uma string: str.length
+// acessando um caractere: str[index]
+// i++ i+=1 i=i+1
+// i-- i-=1 i=i-1
+
+// const numero = prompt("Digite um número: ");
+
+/* let fatorial = 1;
+  for (let i = 1; i <= numero; i++) {
+    fatorial *= i;
+  }
+console.log(fatorial);
+*/
+
+/*
+let contador = 1;
+const numero = prompt("Digite um número: ");
+let fatorial = 1;
+while (contador <= numero) {
+  fatorial *= contador;
+  contador++;
+}
+console.log(fatorial);
+*/
